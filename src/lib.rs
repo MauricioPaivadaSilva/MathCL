3df9cbadb14f641d9ec6f64bb61655486b8b67c0
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-10-26 12:11:26 -0300

    Update lib.rs
    Added basic functions in lib. as well as their tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn sum(mut num: f32, n: u32) -> f32 {
+    let mut i: u32 = 0;
+    let start: f32 = num;
+    while i < n {
+        num += start;
+        i += 1;
+    }
+    num
+}
+
+pub fn fat(mut num: u32) -> u32 {
+    if num == 0 || num == 1 {
+        num = 1;
+    } else {
+        let mut term: u32 = num - 1;
+        while term >= 1 {
+            num *= term;
+            term -= 1;
+        }
+    }
+    num
+}
+
+pub fn pow(mut num: f32, n: i32) -> f32 {
+    if n < 0 {
+        let start: f32 = num;
+        let mut i: i32 = -1;
+        num = 1.0/num;
+        while i > n {
+            num *= 1.0/start;
+            i -= 1;
+        }
+    } else if n == 0 {
+        num = 1.0;
+    } else {
+        let start: f32 = num;
+        let mut i: i32 = 1;
+        while i < n {
+            num *= start;
+            i += 1;
+        }
+    }
+    num
+}
+
+pub fn module(mut num: f32) -> f32 {
+    if num < 0.0 {
+        num *= -1.0;
+    }
+    num
 }
 
 #[cfg(test)]
@@ -7,8 +55,77 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn sum_1(){
+        assert_eq!(sum(1.0, 1), 2.0)
+    }
+
+    #[test]
+    fn sum_2(){
+        assert_eq!(sum(1.0, 10), 11.0)
+    }
+
+    #[test]
+    fn sum_3(){
+        assert_eq!(sum(3.0, 3), 12.0)
+    }
+
+    #[test]
+    fn sum_4(){
+        assert_eq!(sum(-3.0, 3), -12.0)
+    }
+
+    #[test]
+    fn fat_1() {
+        assert_eq!(fat(0), 1)
+    }
+
+    #[test]
+    fn fat_2() {
+        assert_eq!(fat(1), 1)
+    }
+
+    #[test]
+    fn fat_3() {
+        assert_eq!(fat(5), 120)
+    }
+
+    #[test]
+    fn module_1() {
+        assert_eq!(module(2.0), 2.0)
+    }
+
+    #[test]
+    fn module_2() {
+        assert_eq!(module(0.0), 0.0)
+    }
+
+    #[test]
+    fn module_3() {
+        assert_eq!(module(-442.0), 442.0)
+    }
+
+    #[test]
+    fn pow_1() {
+        assert_eq!(pow(-2.0, 2), 4.0)
+    }
+
+    #[test]
+    fn pow_2() {
+        assert_eq!(pow(-2.0, 3), -8.0)
+    }
+
+    #[test]
+    fn pow_3() {
+        assert_eq!(pow(19.0, 0), 1.0)
+    }
+
+    #[test]
+    fn pow_4() {
+        assert_eq!(pow(2.0, -2), 0.25)
+    }
+
+    #[test]
+    fn pow_5() {
+        assert_eq!(pow(-2.0, -3), -0.125)
     }
 }
