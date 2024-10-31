bb340cb06b52aaf1534eba5fd0c12ad50fe91e7b
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-10-31 16:56:52 -0300

    Combinatorics module started.

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,11 @@ pub mod basic {
     }
 
     pub fn sum(mut num: f32, n: u32) -> f32 {
-        let mut i: u32 = 0;
+        let mut i: u32 = 1;
         let start: f32 = num;
-        while i < n {
+        while i <= n {
             num += start;
             i += 1;
-        }
-        num
-    }
-
-    pub fn fat(mut num: u32) -> u32 {
-        if num == 0 || num == 1 {
-            num = 1;
-        } else {
-            let mut term: u32 = num - 1;
-            while term >= 1 {
-                num *= term;
-                term -= 1;
-            }
         }
         num
     }
@@ -68,8 +55,57 @@ pub mod basic {
     }
 }
 
+pub mod combinate {
+    use crate::basic::pow;
+
+    pub fn fat(mut num: u32) -> u32 {
+        if num == 0 || num == 1 {
+            num = 1;
+        } else {
+            let mut term: u32 = num - 1;
+            while term >= 1 {
+                num *= term;
+                term -= 1;
+            }
+        }
+        num
+    }
+
+    pub fn awr(num: u32, r: u32) -> u32 {
+        let arg: f32;
+        if r != 0 {
+            arg = pow(num as f32, r as i32);
+        } else {
+            panic!("\n\tErr ::> argument r is null\n");
+        }
+        arg as u32
+    }
+
+    pub fn arrangement(num: u32, r: u32) -> u32 {
+        let mut arg: u32;
+        if r == 1 {
+            arg = 1;
+        } else if 1 <= r && r <= num {
+            arg = num;
+            let mut cont: u32 = 1;
+            while cont < r && r <= num {
+                arg *= num - cont;
+                cont += 1;
+            }
+        } else {
+            panic!("\n\tErr ::> argument r is null\n");
+        }
+        arg
+    }
+
+    pub fn permutation(num: u32) -> u32 {
+        let arg: u32 = fat(num);
+        arg
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod basic_test {
     use super::*;
 
     #[test]
@@ -90,21 +126,6 @@ mod tests {
     #[test]
     fn sum_4(){
         assert_eq!(basic::sum(-3.0, 3), -12.0)
-    }
-
-    #[test]
-    fn fat_1() {
-        assert_eq!(basic::fat(0), 1)
-    }
-
-    #[test]
-    fn fat_2() {
-        assert_eq!(basic::fat(1), 1)
-    }
-
-    #[test]
-    fn fat_3() {
-        assert_eq!(basic::fat(5), 120)
     }
 
     #[test]
@@ -160,5 +181,55 @@ mod tests {
     #[test]
     fn sqrt_3() {
         assert_eq!(basic::sqrt(9.0, 2), 3.0)
+    }
+}
+
+#[cfg(test)]
+mod combinate_test {
+    use super::*;
+
+    #[test]
+    fn fat_1() {
+        assert_eq!(combinate::fat(0), 1)
+    }
+
+    #[test]
+    fn fat_2() {
+        assert_eq!(combinate::fat(1), 1)
+    }
+
+    #[test]
+    fn fat_3() {
+        assert_eq!(combinate::fat(5), 120)
+    }
+
+    #[test]
+    fn awr_1() {
+        assert_eq!(combinate::awr(52, 3), 140_608)
+    }
+
+    #[test]
+    fn awr_2() {
+        assert_eq!(combinate::awr(2, 3), 8)
+    }
+
+    #[test]
+    fn awr_3() {
+        assert_eq!(combinate::awr(1, 1), 1)
+    }
+
+    #[test]
+    fn arrangement_1() {
+        assert_eq!(combinate::arrangement(52, 3), 132_600)
+    }
+
+    #[test]
+    fn arrangement_2() {
+        assert_eq!(combinate::arrangement(4, 3), 24)
+    }
+
+    #[test]
+    fn arrangement_3() {
+        assert_eq!(combinate::arrangement(37, 4), 1_585_080)
     }
 }
